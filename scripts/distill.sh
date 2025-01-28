3a30b44c1ce2803505194280476a1f844c576f6d
lucascimeca <luca.scimeca@live.com>
2025-01-28 15:17:40 -0500

    stopped tracking wandb

--- scripts/distill.sh
+++ scripts/distill.sh
@@ -57,5 +57,5 @@ unset CUDA_VISIBLE_DEVICES
 ## run the following line to run prior code -- note, the path assume you run the files from the "scripts" folder
 python ../distill.py --exp sd3_align --distilled_ckpt_path ./distilled/ \
                      --teacher_ckpt_filename sd3_a_green_car.pth \
-                     --prompt "A car" --reward_prompt "A green car"
+                     --prompt "A car" --reward_prompt "A green car" \
                      --teacher_ckpt_path ./pretrained/ --wandb_track True
